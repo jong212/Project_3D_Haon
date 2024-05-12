7a2994b0f2678ceb83678de1b3a6d0ed2e680fcf
jong212 <104608917+jong212@users.noreply.github.com>
2024-05-12 22:46:54 +0900

    몬스터 순찰, 유휴, 공격, 추격 FSM 작업완료

--- Assets/Prefabs/AJH/monster/AttackState.cs
+++ Assets/Prefabs/AJH/monster/AttackState.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class test : StateMachineBehaviour
+public class AttackState : StateMachineBehaviour
 {
+    Transform player;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.LookAt(player);
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        if (distance > 3.5)
+            animator.SetBool("isAttacking", false);
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        /*animator.SetInteger("skillA", -1);*/
+        
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
