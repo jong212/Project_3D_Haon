e9e3700398e43bbe026d2f3dc9fe39a12630cad9
rainrarn <tie314@naver.com>
2024-05-13 18:03:07 +0900

    UI 작업

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -2,7 +2,6 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 
 public class LobbyController : MonoBehaviour
