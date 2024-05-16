4bd3caba709c49c2e717cbe406bdbfba6bf8ef97
rainrarn <tie314@naver.com>
2024-05-16 18:04:33 +0900

    [Update]재커밋

--- Assets/Scripts/TutorialKeyController.cs
+++ Assets/Scripts/TutorialKeyController.cs
@@ -19,9 +19,18 @@ public class TutorialKeyController : MonoBehaviour
     
     void Update()
     {
-        transform.LookAt(Camera);
+        //transform.LookAt(Camera);
         if (Input.GetKeyDown(KeyCode.A)) AKey.color = Color.red;
         if (Input.GetKeyUp(KeyCode.A)) AKey.color = Color.white;
+
+        if (Input.GetKeyDown(KeyCode.S)) SKey.color = Color.red;
+        if (Input.GetKeyUp(KeyCode.S)) SKey.color = Color.white;
+
+        if (Input.GetKeyDown(KeyCode.D)) DKey.color = Color.red;
+        if (Input.GetKeyUp(KeyCode.D)) DKey.color = Color.white;
+
+        if (Input.GetKeyDown(KeyCode.W)) WKey.color = Color.red;
+        if (Input.GetKeyUp(KeyCode.W)) WKey.color = Color.white;
 
     }
 
