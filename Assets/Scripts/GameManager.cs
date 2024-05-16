1b3eb36b6a65f0edb46173bf0864e139a783466e
Hyeon616 <whdgus616@gmail.com>
2024-05-16 17:49:56 +0900

    NetCode 작업중(Scene에 Host Player, Client Player 생성)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+
+        NetworkManager.Singleton.NetworkConfig.ConnectionApproval = true;
+        if (RelayManager.Instance.IsHost)
+        {
+            NetworkManager.Singleton.ConnectionApprovalCallback = ConnectionApproval;
+            (byte[] allocationId, byte[] key, byte[] connectionData, string ip, int port) = RelayManager.Instance.GetHostConnectionInfo();
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(ip, (ushort)port, allocationId, key, connectionData, true);
+            NetworkManager.Singleton.StartHost();
+
+        }
+        else
+        {
+            (byte[] allocationId, byte[] key, byte[] connectionData, byte[] hostConnectionData, string ip, int port) = RelayManager.Instance.GetClientConnectionInfo();
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(ip, (ushort)port, allocationId, key, connectionData, hostConnectionData, true);
+            NetworkManager.Singleton.StartClient();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ConnectionApproval(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
     {
-        
+        response.Approved = true;
+        response.CreatePlayerObject = true;
+        response.Pending = false;
     }
 }
