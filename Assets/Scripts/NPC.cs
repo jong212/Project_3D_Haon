63d48b2ed42dea14029ccf53199920198baf59c4
rainrarn <tie314@naver.com>
2024-05-20 05:25:31 +0900

    [Update] TutorialScene

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -17,37 +17,43 @@ public class NPC : MonoBehaviour
     public int talkIndex = 0;
     public GameObject talkBullon;
     public GameObject monsterBullon;
-    public GameObject talkBallun2;
     public GameObject talktext1_1;
     public GameObject talktext1_2;
     public GameObject talktext1_3;
     public GameObject talktext1_4;
 
-
-
     public GameObject talktext2_1;
     public GameObject talktext2_2;
     public GameObject talktext2_3;
+    public GameObject talktext2_4;
+
+    public GameObject talktext3;
 
     public GameObject MoveInfo;
     public GameObject AttackInfo;
     public Transform tree1;
     public Transform tree2;
+    public BoxCollider treecollider;
+
     public GameObject portalDoor;
     
     private bool IsNear=false;
     public CinemachineVirtualCamera talkcamera;
     public CinemachineVirtualCamera portalcamera;
     public CinemachineVirtualCamera moncamera;
+    public CinemachineVirtualCamera chestcamera;
+    public CinemachineVirtualCamera chartercamera;
 
     public GameObject Monster;
     public GameObject player;
-
+    public GameObject chest;
+    public ParticleSystem chesteffect;
 
    
     
     void Update()
     {
+        //ëŒ€í™”1
         if(IsNear==true&& talkCount == 0&&talkIndex==0&&Input.GetKeyDown(KeyCode.F))
         {
             talkcamera.Priority = 2;
@@ -68,65 +74,165 @@ public class NPC : MonoBehaviour
         {
             talktext1_2.SetActive(false);
             talktext1_3.SetActive(true);
-            Invoke("MonsterBallunAppear", 3f);
+            Invoke("MonsterBallunAppear", 1f);
             talkCount += 1;
         }
-        else if (IsNear == true && talkIndex == 3 && talkIndex == 0 && Input.GetKeyDown(KeyCode.F))
+        else if (IsNear == true && talkCount == 3 && talkIndex == 0 && Input.GetKeyDown(KeyCode.F))
         {
+            monsterBullon.SetActive(false);
             moncamera.Priority = 3;
             talkBullon.SetActive(false);
-            talkBallun2.SetActive(true);
             AttackInfo.SetActive(true);
             Invoke("MonsterAppear", 1.5f);
             talkCount += 1;
         }
-        else if (IsNear == true && talkIndex == 4 && talkIndex == 0 && Input.GetKeyDown(KeyCode.F))
+        else if (IsNear == true && talkCount == 4 && talkIndex == 0 && Input.GetKeyDown(KeyCode.F))
         {
+            monsterBullon.SetActive(false);
             moncamera.Priority = 0;
-            talkBullon.SetActive(true);
-            talkBallun2.SetActive(false);
+            Invoke("talkballonAppear", 2f);
             talktext1_3.SetActive(false);
             talktext1_4.SetActive(true);
             talkCount += 1;
         }
         else if (IsNear == true && talkCount == 5 && talkIndex == 0 && Input.GetKeyDown(KeyCode.F))
         {
-            talkBullon.SetActive(false);            
+            talktext1_4.SetActive(false);
+            talktext2_1.SetActive(true);
+            talkBullon.SetActive(false); 
             talkcamera.Priority = 0;
             player.GetComponent<PlayerInput>().enabled = true;
             talkIndex = 1;
-            talkCount =0;
+            talkCount = 0;
         }
 
+        //ëŒ€í™”2
+        else if (IsNear == true && talkCount == 0 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            talkcamera.Priority = 2;
+            TalkButton.SetActive(false);
+            AttackInfo.SetActive(false);
+            Invoke("talkballonAppear", 2f);          
+            player.GetComponent<PlayerInput>().enabled = false;
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 1 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            talktext2_1.SetActive(false);
+            talktext2_2.SetActive(true);
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 2 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            chestcamera.Priority = 3;
+            talkBullon.SetActive(false);
+            Invoke("ChestAppear", 2f);
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 3 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            chestcamera.Priority = 0;
+            Invoke("talkballonAppear", 2f);
+            talktext2_2.SetActive(false);
+            talktext2_3.SetActive(true);
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 4 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            portalcamera.Priority = 3;
+            Invoke("TreeOpen", 2f);
+            StartCoroutine(ShakeCamera());
+            talkBullon.SetActive(false);
+
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 5 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            portalcamera.Priority = 0;
+            Invoke("talkballonAppear", 2f);
+            talktext2_3.SetActive(false);
+            talktext2_4.SetActive(true);
+
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 6 && talkIndex == 1 && Input.GetKeyDown(KeyCode.F))
+        {
+            talktext2_4.SetActive(false);
+            talktext3.SetActive(true);
+            talkBullon.SetActive(false);
+            talkcamera.Priority = 0;
+            player.GetComponent<PlayerInput>().enabled = true;
+            talkIndex = 2;
+            talkCount = 0;
+        }
         
-        //else if (IsNear == true && talkCount == 2 && Input.GetKeyDown(KeyCode.F))
-        //{
-        //    moncamera.Priority = 0;
-        //    portalcamera.Priority = 11;
-        //    talkCount += 1;
-        //    Invoke("TreeOpen", 2.5f);
-        //}
-        //if (IsNear == true && Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    talkcamera.Priority = 0;
-        //    moncamera.Priority = 0;
-        //    portalcamera.Priority = 0;
-        //    player.GetComponent<PlayerInput>().enabled = true;
-        //    talkCount = 0;
+        //ëŒ€í™”3
+        else if (IsNear == true && talkCount == 0 && talkIndex == 2 && Input.GetKeyDown(KeyCode.F))
+        {
+            talkcamera.Priority = 2;
+            TalkButton.SetActive(true);
+            Invoke("talkballonAppear", 2f);
+            player.GetComponent<PlayerInput>().enabled = false;
+            talkCount += 1;
+        }
+        else if (IsNear == true && talkCount == 1 && talkIndex == 2 && Input.GetKeyDown(KeyCode.F))
+        {
+            talkBullon.SetActive(false);
+            talkcamera.Priority = 0;
+            player.GetComponent<PlayerInput>().enabled = true;
+            talkCount = 0;
+        }
 
-        //}
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            portalcamera.Priority = 3;
+            Invoke("TreeOpen", 2f);
+            StartCoroutine(ShakeCamera());
+        }
 
+    }
+    IEnumerator ShakeBullon()
+    {
+        float time = 3f;
+        float shakePower = 500f;
+        Vector3 origin = monsterBullon.transform.position;
+
+        while(time>0f)
+        {
+            time -= 0.1f;
+            monsterBullon.transform.position = origin + (Vector3)Random.insideUnitCircle * shakePower * time;
+            yield return null;
+        }
+
+        monsterBullon.transform.position = origin;
+    }
+    IEnumerator ShakeCamera()
+    {
+        float time = 45f;
+        float shakePower = 0.1f;
+        Vector3 origin = chartercamera.transform.position;
+
+        while (time > 0f)
+        {
+            time -= 0.05f;
+            chartercamera.transform.position = origin + (Vector3)Random.insideUnitCircle * shakePower;// * time;
+            yield return null;
+        }
+
+        chartercamera.transform.position = origin;
     }
     void TreeOpen()
     {
         tree1.transform.DOMove(new Vector3(7f, 0f, 7f), 5.0f);
         tree2.transform.DOMove(new Vector3(12f, 0f, 2.5f), 5.0f);
         portalDoor.SetActive(true);
+        tree1.GetComponent<BoxCollider>().enabled=false;
     }
     void MonsterBallunAppear()
     {
         
         monsterBullon.SetActive(true);
+        StartCoroutine(ShakeBullon());
     }
     void MonsterAppear()
     {
@@ -137,11 +243,18 @@ public class NPC : MonoBehaviour
     {
         talkBullon.SetActive(true);
     }
+    void ChestAppear()
+    {
+        chest.SetActive(true);
+        chesteffect.Play();
+
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("¸»°É·¡");
+            
             IsNear = true;
             TalkButton.SetActive(true);
         }
@@ -150,7 +263,7 @@ public class NPC : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("ÀßÀÖ¾î");
+            
             IsNear = false;
             TalkButton.SetActive(false);
         }
