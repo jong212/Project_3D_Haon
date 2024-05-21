d5bba0e6ed941c0648b9a7047159410e3198ff82
조준후 <jogusals4601@naver.com>
2024-05-21 11:25:02 +0900

    [Update] 밀어지는 오브젝트 추가 및 추가 수정 작업

--- Assets/Scripts/Traps/TrapDamaged.cs
+++ Assets/Scripts/Traps/TrapDamaged.cs
@@ -4,6 +4,7 @@ public class TrapDamaged : MonoBehaviour
 {
     public playerAnimator player;
     public int damageIntPoint = 1;
+    public float sladeSpeed = 1;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -11,6 +12,10 @@ public class TrapDamaged : MonoBehaviour
         {
             player.TakeDamage(damageIntPoint);
             Debug.Log("Trap Damaged");
+        }
+        else if(hit.gameObject.name == "SladeRock")
+        {
+            hit.gameObject.transform.Translate(gameObject.transform.forward * sladeSpeed * Time.deltaTime);
         }
     }
 }
