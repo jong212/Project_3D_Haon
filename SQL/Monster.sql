abc2db5c22e0de4160f9bb695b17cf9de834f56f
Hyeon616 <whdgus616@gmail.com>
2024-05-24 06:46:18 +0900

    누락된 Query 추가

---
+++ SQL/Monster.sql
@@ -0,0 +1,16 @@
+INSERT INTO Monsters (Name, Health, AttackPower)
+VALUES 
+('Goblin', 100, 10),
+('Orc', 150, 15),
+('Troll', 200, 20),
+('Skeleton', 80, 8),
+('Zombie', 90, 9),
+('Vampire', 110, 12),
+('Werewolf', 130, 14),
+('Ghoul', 70, 7),
+('Imp', 60, 6),
+('Wraith', 85, 10);
+
+INSERT INTO Monsters (Name, Health, AttackPower)
+VALUES 
+('Dragon', 1000, 50);
---
+++ SQL/Player.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    PasswordHash NVARCHAR(256) NOT NULL
+);
+
+
+CREATE TABLE Players (
+    PlayerID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL FOREIGN KEY REFERENCES Users(UserID),
+    PlayerName NVARCHAR(50) NOT NULL,
+    Gems INT DEFAULT 0,
+    Coins INT DEFAULT 0,
+    MaxHealth INT DEFAULT 500 NOT NULL,
+    HealthEnhancement INT DEFAULT 0,
+    AttackPower INT DEFAULT 40 NOT NULL,
+    AttackEnhancement INT DEFAULT 0,
+    WeaponEnhancement INT DEFAULT 0,
+    ArmorEnhancement INT DEFAULT 0
+);
+
+CREATE TABLE Monsters (
+    MonsterID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(50) NOT NULL,
+    Health INT NOT NULL,
+    AttackPower INT NOT NULL
+);
