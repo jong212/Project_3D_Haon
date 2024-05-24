98237fd0dff0abde8f2cb9d05c799eab7b7a976c
jonghwa <github.com/jong212>
2024-05-24 20:41:14 +0900

    작업중

--- Assets/Scripts/Item/MoveGate/UpDownMoveGate.cs
+++ Assets/Scripts/Item/MoveGate/UpDownMoveGate.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 public class UpDownMoveGate : MonoBehaviour
 {
     public GameObject gate;
+    public GameObject Canvas_Boss;
+
     public float speed = 1;
     public bool down = false;
+    public bool bossStart = false;
     public float upDownStopSecond = 0;
-    [SerializeField] private BgmManager bgmManager;
 
+    [SerializeField] private BgmManager bgmManager;
+    [SerializeField] private Boss boss;
 
     [SerializeField] private bool isMoving = false;
     private int point = 0;
 
 
+    private void Start()
+    {
+        
+    }
     private void Update()
     {
         MoveGate();
@@ -33,9 +41,17 @@ public class UpDownMoveGate : MonoBehaviour
         {
             gate.transform.Translate(Vector3.down * speed * Time.deltaTime);
             Invoke("DownActive", upDownStopSecond);
+         
+
         }
         else if (isMoving && point == 0 && !down)
         {
+            if (!bossStart)
+            {
+                boss.bosssRoomStartCheck = true;
+                bossStart = true;
+                Canvas_Boss.active = true;
+            }
             gate.transform.Translate(Vector3.up * speed * Time.deltaTime);
             Invoke("UpActive", upDownStopSecond);
         }
